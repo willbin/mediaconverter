4d46bdfa9e8bc524f715666cdf04bdd30d386627
Will Wei <will.wei@ringcentral.com>
2019-04-14 18:07:50 +0800

    Init

---
+++ mediaconverter/MediaConverter/transcoding.h
@@ -0,0 +1,14 @@
+//
+//  transcoding.h
+//  MediaConverter
+//
+//  Created by Jackie Ou on 15/11/2017.
+//  Copyright © 2017 Jackie Ou. All rights reserved.
+//
+
+#ifndef transcoding_h
+#define transcoding_h
+
+int transcoding(char* input_filename, char* output_filename, void* callback);
+
+#endif /* transcoding_h */
